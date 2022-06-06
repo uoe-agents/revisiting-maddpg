ef042b882cfb1fa100c7d2353854303ee5b47d1c
callumtilbury <callum@tilbury.co.za>
2022-06-06 11:45:17 +0100

    Renamed temp->temperature for clarity (NOT 'temporary')

--- utils.py
+++ utils.py
@@ -8,10 +8,10 @@ def gumbel_noise(shape, key, eps=1e-20):
     uu = jrand.uniform(key, shape)
     return -jnp.log( -jnp.log( uu + eps) + eps)
 
-def gumbel_softmax(logits, key, temp=1.0, st=True):
+def gumbel_softmax(logits, key, temperature=1.0, st=True):
     yy = logits + gumbel_noise(logits.shape, key)
     if st:
         return jnn.one_hot(jnp.argmax(yy), yy.shape[0])
     else:
-        return jnn.softmax(yy / temp) #, dim=1) #TODO: Sort out batching
+        return jnn.softmax(yy / temperature) #, dim=1) #TODO: Sort out batching
 
