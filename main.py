d7ebd9fe900cafd265fd46cc42e493fb6c329ab7
callumtilbury <callum@tilbury.co.za>
2022-06-08 13:29:17 +0100

    Basic environment loop working, along with simple arg parsing.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import argparse
+from tqdm import tqdm
 #from pettingzoo.mpe import simple_adversary_v2
 import gym, rware
 from buffer import ReplayBuffer
@@ -5,31 +7,67 @@ import numpy as np
 from make_env import make_env
 import jax
 import jax.random as jrand
+from agent import Agent
 
 jax.config.update('jax_platform_name', 'cpu')
 
-env = make_env("simple_adversary")
-# env = gym.make("rware-small-4ag-v1")
-N = len(env.observation_space)
-obs = env.reset()
-observation_dims = np.array([os.shape[0] for os in env.observation_space])
+def train(config: argparse.Namespace):
+    env = make_env(config.env)
+    n_agents = env.n_agents
+    observation_dims = np.array([obs.shape[0] for obs in env.observation_space])
+    buffer = ReplayBuffer(10e6, observation_dims) # TODO: change format of the replay buffer input??
 
-env.step(env.action_space.sample())
+    agents = [
+        Agent(
+            agent_idx=ii,
+            observation_space=env.observation_space,
+            action_space=env.action_space,
+            # TODO: Consider changing this to **config
+            hidden_dim_width=config.hidden_dim_width,
+            critic_lr=config.critic_lr,
+            actor_lr=config.actor_lr,
+        )
+        for ii in range(n_agents)
+    ]
 
-buffer = ReplayBuffer(10e6, observation_dims)
 
-for ii in range(100):
-    acts = env.action_space.sample()
-    nobs, rwds, dones, _ = env.step(acts)
+    for epi_i in tqdm(range(config.n_episodes)):
+        obs = env.reset()
+        for _ in range(config.episode_length):
+            if (config.render): env.render()
+            #acts = env.action_space.sample() # TODO: acts should come from agent policies
+            acts = [agents[ii].act(obs[ii]) for ii in range(n_agents)]
+            nobs, rwds, dones, _ = env.step(acts)
+
+            buffer.store(
+                obs=np.concatenate(obs),
+                acts=acts,
+                rwds=rwds,
+                nobs=np.concatenate(nobs),
+                dones=dones,
+            )
+
+            obs = nobs
+
+            if (buffer.ready(config.batch_size)):
+                ...
+                # MADDPG.update()
+
+    env.close()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--env", default="simple_adversary")
+    parser.add_argument("--seed", default=1, type=int)
+    parser.add_argument("--n_episodes", default=10, type=int)
+    parser.add_argument("--episode_length", default=100, type=int)
+    parser.add_argument("--batch_size", default=1024, type=int)
+    parser.add_argument("--render", default=False, type=bool)
+    parser.add_argument("--hidden_dim_width", default=64, type=int)
+    parser.add_argument("--critic_lr", default=1e-3, type=float)
+    parser.add_argument("--actor_lr", default=1e-4, type=float)
+
+    config = parser.parse_args()
     
-    buffer.store(
-        obs=np.concatenate(obs),
-        acts=acts,
-        rwds=rwds,
-        nobs=np.concatenate(nobs),
-        dones=dones,
-    )
-
-    obs = nobs
-
-print(buffer.sample(5, jrand.PRNGKey(123)))
+    train(config)
