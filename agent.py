bef897fa36928b67f3eed92851183fd56848ad73
callumtilbury <callum@tilbury.co.za>
2022-06-08 13:27:42 +0100

    Basic structuring of agent will ability to select action based on observations into policy network.

--- agent.py
+++ agent.py
@@ -1,53 +1,99 @@
-from networks import ActorNetwork, CriticNetwork
+from copy import deepcopy
+from torch import eq
 import jax.random as jrand
+import jax.numpy as jnp
+import jax.nn as jnn
 import utils
+import haiku as hk
+import equinox as eqx
+import optax
+
+KEY = jrand.PRNGKey(123456)
 
 class Agent:
-    def __init__(self, actor_dim, critic_dim, n_agents, n_actions, agent_idx, key,
-        fc_dims=[64,64], gamma=0.95, alpha_actor=0.01, alpha_critic=0.01, tau=0.01):
-        self.gamma = gamma
-        self.tau = tau
-        self.n_actions = n_actions
-        self.agent_name = f"agent_{agent_idx}"
+    # def __init__(self, actor_dim, critic_dim, n_agents, n_actions, agent_idx, key,
+        # fc_dims=[64,64], gamma=0.95, alpha_actor=0.01, alpha_critic=0.01, tau=0.01):
+    def __init__(self,
+        agent_idx,
+        observation_space,
+        action_space,
+        hidden_dim_width,
+        critic_lr,
+        actor_lr,
+        tau=0.01,
+        # more TODO
+    ):
+        self.agent_idx = agent_idx
+        # self.critic_lr = critic_lr,
+        # self.actor_lr = actor_lr,
+        # -----------
+        _, *subkeys = jrand.split(KEY, num=3)
 
-        ba_key, bc_key, ta_key, tc_key, self.act_key = jrand.split(key, 5)
+        # ***** POLICY *****
+        policy_in_size = observation_space[self.agent_idx].shape[0]
+        policy_out_size = action_space[self.agent_idx].n
 
-        self.behaviour_actor = ActorNetwork(
-            obs_dim=actor_dim,
-            n_actions=n_actions,
-            fc_dims=fc_dims,
-            lr=alpha_actor,
-            key=ba_key,
+        self.behaviour_policy = eqx.nn.MLP(
+            in_size=policy_in_size,
+            width_size=hidden_dim_width,
+            out_size=policy_out_size,
+            depth=1,
+            activation=jnn.relu,
+            final_activation=jnn.log_softmax, # Gumbel-Softmax takes in log-probabilities, right??
+            key=subkeys[0],
+        )
+        self.target_policy = deepcopy(self.behaviour_policy) #  TODO: should this use a different rng key??
+        # ***** ****** *****
+
+        # ***** CRITIC *****
+        critic_in_size = \
+            sum([obs.shape[0] for obs in observation_space]) + \
+            sum([act.n for act in action_space])
+
+        self.behaviour_critic = eqx.nn.MLP(
+            in_size=critic_in_size,
+            width_size=hidden_dim_width,
+            out_size=1, # Critic outputs a scalar value
+            depth=1,
+            activation=jnn.relu,
+            key=subkeys[1],
+        )
+        self.target_critic = deepcopy(self.behaviour_critic) # TODO: as above??
+        # ***** ****** *****
+
+        # OPTIMISERS
+        self.policy_optim = optax.adam(actor_lr)
+        self.policy_optim_state = self.policy_optim.init(
+            eqx.filter(self.policy_optim, eqx.is_inexact_array)
         )
 
-        self.behaviour_critic = CriticNetwork(
-            obs_dims=critic_dim,
-            n_agents=n_agents,
-            n_actions=n_actions,
-            fc_dims=fc_dims,
-            lr=alpha_critic,
-            key=bc_key,
-        )
-
-        self.target_actor = ActorNetwork(
-            obs_dim=actor_dim,
-            n_actions=n_actions,
-            fc_dims=fc_dims,
-            lr=alpha_actor,
-            key=ta_key, # Should the targets' weights be initialised to behaviour networks?
-        )
-
-        self.target_critic = CriticNetwork(
-            obs_dims=critic_dim,
-            n_agents=n_agents,
-            n_actions=n_actions,
-            fc_dims=fc_dims,
-            lr=alpha_critic,
-            key=tc_key, # TODO: see above
+        self.critic_optim = optax.adam(critic_lr)
+        self.critic_optim_state = self.critic_optim.init(
+            eqx.filter(self.critic_optim, eqx.is_inexact_array)
         )
 
     def act(self, obs):
-        ...
-        # actions = self.behaviour_actor(obs)
-        # noise = jrand.normal(self.act_key, shape=actions.shape)
-        # return actions + noise
+        # TODO fix PRNG key
+        _, act_key = jrand.split(KEY)
+        actions_one_hot = utils.gumbel_softmax(self.behaviour_policy(obs), act_key, temperature=0.8, st=True)
+        return jnp.argmax(actions_one_hot)
+
+    def update(self, key):
+        return None # temp
+        loss_fn = eqx.filter_value_and_grad(...)#(batch_loss_fn????)
+        loss, grads = loss_fn(self.behaviour_policy)#, weight, init
+        updates, self.policy_optim_state = self.policy_optim.update(grads, self.policy_optim_state)
+        self.behaviour_policy = eqx.apply_updates(self.behaviour_policy, updates)
+        key = jrand.split(key, 1)[0]
+
+        print(loss)
+        return None
+        
+
+    # def hard_update(self, source):
+    #     for target_param, source_param in zip(self.parameters(), source.parameters()):
+    #         target_param.data.copy_(source_param.data)
+
+    # def soft_update(self, source, t):
+    #     for target_param, source_param in zip(self.parameters(), source.parameters()):
+    #         target_param.data.copy_((1 - t) * target_param.data + t * source_param.data)
