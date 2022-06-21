dbb99c6a8d4a0190201609748c0cebc9990b24e4
callumtilbury <callum@tilbury.co.za>
2022-06-21 16:20:24 +0100

    Created a GumbelSoftmax Haiku module and incorporated it into CriticNetworks.

--- networks.py
+++ networks.py
@@ -2,22 +2,26 @@ from typing import List
 import haiku as hk
 import jax.nn as jnn
 import jax.numpy as jnp
+import utils
 
 # TODO: PASS HIDDEN DIMS & DEPTH AS A PARAM
 # TODO: Typing
 
 class ActorNetwork(hk.Module):
-    def __init__(self, n_actions):
+    def __init__(self, n_actions, key):
         super(ActorNetwork, self).__init__()
         self.n_actions = n_actions
+        self.key = key
 
     def __call__(self, obs: jnp.ndarray) -> jnp.DeviceArray:
         net = hk.Sequential(layers=[
             hk.Linear(100), # TODO: w_init = ?
             jnn.relu,
             hk.Linear(self.n_actions),
+            jnn.log_softmax,
+            utils.GumbelSoftmax(temperature=0.75, key=self.key)
         ])
-        return jnn.log_softmax(net(obs)) #  TODO: Gumbel-softmax etc
+        return net(obs) #  TODO: Gumbel-softmax etc
 
 class CriticNetwork(hk.Module):
     def __init__(self):
--- utils.py
+++ utils.py
@@ -5,6 +5,7 @@ import jax.nn as jnn
 import jax.lax as jlax
 import equinox as eqx
 import haiku as hk
+from typing import Optional
 
 def gumbel_softmax(logits, key, temperature, st=True):
     # TODO: Explore how this could be written with a custom jax derivative expression (jax.custom_jvp)
@@ -18,6 +19,37 @@ def gumbel_softmax(logits, key, temperature, st=True):
         return y_hard - jlax.stop_gradient(y_soft) + y_soft
     else:
         return y_soft
+
+class GumbelSoftmax(hk.Module):
+    """TODO"""
+
+    def __init__(
+        self,
+        temperature: float,
+        key,
+        straight_through: Optional[bool] = True,
+        name: Optional[str] = None,
+    ):
+        super().__init__(name=name)
+        self.temperature = temperature
+        self.key = key
+        self.st = straight_through
+
+    def __call__(
+        self,
+        logits: jnp.ndarray,
+        *args,
+    ) -> jnp.ndarray:    
+        self.key, subkey = jrand.split(self.key)
+        gumbels = jrand.gumbel(subkey, logits.shape)
+        y_soft = jnn.softmax( (logits + gumbels) / self.temperature )
+    
+        if self.st:
+            y_hard = jnn.one_hot(jnp.argmax(y_soft), num_classes=logits.shape[-1])
+            # See https://github.com/pytorch/pytorch/blob/32593ef2dd26e32ed44d3c03d3f5de4a42eb149a/torch/nn/functional.py#L1797
+            return y_hard - jlax.stop_gradient(y_soft) + y_soft
+        else:
+            return y_soft
 
 def _incremental_update(old, new, tau):
     if new is None:
