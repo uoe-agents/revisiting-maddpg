a6e78c777b85a9a239735173070497a6f3d0f21e
callumtilbury <callum@tilbury.co.za>
2022-06-29 14:45:29 +0100

    Added matrix game to envs, but crudely so.

--- env_wrapper.py
+++ env_wrapper.py
@@ -1,4 +1,4 @@
-import gym, rware, lbforaging
+import gym, rware, lbforaging, matrixgames
 from make_env import make_env
 
 def create_env(env_string):
@@ -6,6 +6,8 @@ def create_env(env_string):
     if (env_string.find("Foraging") != -1 or \
         env_string.find("rware") != -1):
         env = gym.make(env_string)
+    elif (env_string.find("matrix") != -1):
+        env = gym.make("climbing-nostate-v0")
     else: # Assume MPE
         env = make_env(env_string)
     
