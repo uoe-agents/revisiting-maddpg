648cf8ef87800d1c921f4b3d5b8c1d27d35d3bfb
callumtilbury <callum@tilbury.co.za>
2022-06-29 14:47:31 +0100

    Fixed 'done' bug, and changed default hyperparams.

--- main.py
+++ main.py
@@ -24,11 +24,15 @@ def play_episode(
     steps = 0
     episode_return = 0
 
-    while not any(dones) and (steps < max_timesteps_per_episode):
+    while not any(dones):# and (steps < max_timesteps_per_episode):
         if (render): env.render()
 
         acts = action_fn(obs)#maddpg.acts(obs)
         nobs, rwds, dones, _ = env.step(acts)
+
+        steps += 1
+        if (steps >= max_timesteps_per_episode):
+            dones = [True] * env.n_agents
 
         buffer.store(
             obs=obs,
@@ -37,21 +41,13 @@ def play_episode(
             nobs=nobs,
             dones=dones,
         )
-
-        steps += 1
         episode_return += sum(rwds)
         obs = nobs
 
-    return episode_return, steps
-    # if buffer.ready() and (steps % steps_per_update == 0): # TODO: improve training interval setup
-    #     sample = buffer.sample()
-    #     maddpg.update(sample)
-
-
+    return episode_return#, steps
 
 def train(config: argparse.Namespace, rng):
     env = create_env(config.env)
-    #n_agents = env.n_agents
     observation_dims = np.array([obs.shape[0] for obs in env.observation_space])
     buffer = ReplayBuffer(
         capacity=10e6,
@@ -67,6 +63,7 @@ def train(config: argparse.Namespace, rng):
         gradient_clip=config.gradient_clip,
         hidden_dim_width=config.hidden_dim_width,
         gamma=config.gamma,
+        gumbel_temp=config.gumbel_temp,
         rng=rng,
     )
 
@@ -92,8 +89,9 @@ def train(config: argparse.Namespace, rng):
                 render=False,
             )
 
-            sample = buffer.sample()
-            maddpg.update(sample)
+            if (not config.disable_training):
+                sample = buffer.sample()
+                maddpg.update(sample)
 
             if (config.eval_freq != 0 and epi_i % config.eval_freq == 0):
                 eval_returns = []
@@ -112,21 +110,22 @@ def train(config: argparse.Namespace, rng):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--env", default="simple_adversary")
+    parser.add_argument("--env", default="simple_speaker_listener")
     parser.add_argument("--seed", default=1, type=int)
     parser.add_argument("--n_episodes", default=25000, type=int)
-    parser.add_argument("--warmup_episodes", default=10000, type=int)
-    parser.add_argument("--timesteps", default=25000, type=int)
-    parser.add_argument("--episode_length", default=50, type=int)
-    parser.add_argument("--steps_per_update", default=100, type=int)
-    parser.add_argument("--batch_size", default=1024, type=int)
-    parser.add_argument("--hidden_dim_width", default=256, type=int)
-    parser.add_argument("--critic_lr", default=1e-2, type=float)
-    parser.add_argument("--actor_lr", default=1e-2, type=float)
+    parser.add_argument("--warmup_episodes", default=400, type=int)
+    #parser.add_argument("--timesteps", default=25000, type=int) # TODO?
+    parser.add_argument("--episode_length", default=25, type=int)
+    #parser.add_argument("--steps_per_update", default=100, type=int) # TODO: Bring back?
+    parser.add_argument("--batch_size", default=512, type=int)
+    parser.add_argument("--hidden_dim_width", default=64, type=int) # TODO: Pass this as a tuple and unpack into n layers in network creation?
+    parser.add_argument("--critic_lr", default=3e-4, type=float)
+    parser.add_argument("--actor_lr", default=3e-4, type=float)
     parser.add_argument("--gradient_clip", default=1.0, type=float)
     parser.add_argument("--gamma", default=0.95, type=float)
     parser.add_argument("--eval_freq", default=100, type=int)
     parser.add_argument("--eval_iterations", default=100, type=int)
+    parser.add_argument("--gumbel_temp", default=1.0, type=float)
     parser.add_argument("--render", action="store_true")
     parser.add_argument("--disable_training", action="store_true")
     parser.add_argument("--disable_wandb", action="store_true")
