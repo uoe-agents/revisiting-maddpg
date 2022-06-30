f7017d576e6c0a19ee00415616553cd1118180f6
callumtilbury <callum@tilbury.co.za>
2022-06-30 10:16:55 +0100

    Reworked where the Gumbel-Softmax operation occurs, bringing it out of the Haiku module and into the agent code.

--- utils.py
+++ utils.py
@@ -10,13 +10,13 @@ Array = Any
 
 # TODO: Decide whethere 'hard' switch is necessary. By removing control flow, we can jit
 #@jit
-def gumbel_softmax_st(logits: Array, key, tau: float = 1) -> Array:
+def gumbel_softmax_st(logits: Array, key, temperature: float = 1) -> Array:
     """
         Straight-through Gumbel-Softmax Estimator
         See https://github.com/pytorch/pytorch/blob/32593ef2dd26e32ed44d3c03d3f5de4a42eb149a/torch/nn/functional.py#L1797
     """
     gumbels = jrand.gumbel(key, logits.shape)
-    y_soft = jnn.softmax( (logits + gumbels) / tau )
+    y_soft = jnn.softmax( (logits + gumbels) / temperature )
 
     y_hard = jnn.one_hot(jnp.argmax(y_soft), num_classes=logits.shape[-1])
     return y_hard - jlax.stop_gradient(y_soft) + y_soft
