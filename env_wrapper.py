86b1e24bb5a217c8045af4d2052c0461f3d1e936
callumtilbury <callum@tilbury.co.za>
2022-07-01 11:21:48 +0100

    Removed matrix games from main branch.

--- env_wrapper.py
+++ env_wrapper.py
@@ -6,8 +6,6 @@ def create_env(env_string):
     if (env_string.find("Foraging") != -1 or \
         env_string.find("rware") != -1):
         env = gym.make(env_string)
-    elif (env_string.find("matrix") != -1):
-        env = gym.make("climbing-nostate-v0")
     else: # Assume MPE
         env = make_env(env_string)
     
