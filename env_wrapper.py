51b45b6f874396d8e8221dedd848dfa89c602434
callumtilbury <callum@tilbury.co.za>
2022-07-11 15:37:49 +0100

    Registered short-sight environments.

--- env_wrapper.py
+++ env_wrapper.py
@@ -1,5 +1,33 @@
 import gym, rware, lbforaging
 from make_env import make_env
+from gym.envs.registration import register
+
+register(
+    id="Foraging-8x8-2p-2f-2s-c-v2",
+    entry_point="lbforaging.foraging:ForagingEnv",
+    kwargs={
+        "players": 2,
+        "max_player_level": 3,
+        "field_size": (8, 8),
+        "max_food": 2,
+        "sight": 2,
+        "max_episode_steps": 50,
+        "force_coop": True,
+    },
+)
+
+register(
+    id="Foraging-10x10-3p-3f-2s-v2",
+    entry_point="lbforaging.foraging:ForagingEnv",
+    kwargs={
+        "players": 3,
+        "max_player_level": 3,
+        "field_size": (10, 10),
+        "max_food": 3,
+        "sight": 2,
+        "max_episode_steps": 50,
+    },
+)
 
 def create_env(env_string):
     env = ...
