550a6382d7f53eff036d8b5a4d26dcfd652756df
callumtilbury <callum@tilbury.co.za>
2022-08-16 23:42:54 +0100

    Update compute-time script.

--- compute_time.py
+++ compute_time.py
@@ -4,22 +4,43 @@ import timeit
 import numpy as np
 import scipy.stats as st
 
-logits = torch.randn([100,3])
+dims = [3,5,10,50,100,1000]
 
 estimators = {
     "STGS": GE.STGS(1.0),
-    "GRMCK1": GE.GRMCK(1.0, 1),
-    "GRMCK10": GE.GRMCK(1.0, 10),
-    "GRMCK50": GE.GRMCK(1.0, 100),
+    "MCK1": GE.GRMCK(1.0, 1),
+    "MK10": GE.GRMCK(1.0, 10),
+    "MK50": GE.GRMCK(1.0, 100),
     "GST": GE.GST(1.0),
 }
 
 REPEATS = 5
-NUMBER = 500
+NUMBER = 10_000
 
-for ee in estimators.items():
-    times = [time / NUMBER for time in timeit.repeat(lambda: ee[1](logits), repeat=REPEATS, number=NUMBER)]
-    time_mean = np.mean(times)
-    time_error = st.t.interval(0.95, len(times)-1, loc=time_mean, scale=st.sem(times))
+first_time = True
+for dim in dims:
+    logits = torch.randn([1,dim])
+    if first_time:
+        print(str(dim) + ",x",end=",")
+    else:
+        print(str(dim) + ",",end=",")
+    
+    baseline_mean = -1
+    multiplier = -1
 
-    print(f"{ee[0]}: {time_mean * 1e6} ± {(time_error[1] - time_mean) * 1e6} us")
+    for ee in estimators.items():
+        times = [time / NUMBER for time in timeit.repeat(lambda: ee[1](logits), repeat=REPEATS, number=NUMBER)]
+        time_mean = np.mean(times)
+        time_error = st.t.interval(0.95, len(times)-1, loc=time_mean, scale=st.sem(times))
+
+        if ee[0] == "STGS":
+            baseline_mean = time_mean
+            multiplier = 1.00
+        else:
+            multiplier = np.round(time_mean / baseline_mean, 2)
+        
+        mean = np.round(time_mean * 1e6, 2)
+        error =  np.round((time_error[1] - time_mean) * 1e6, 2)
+        print(mean,error,multiplier,sep=",",end=",")
+
+    print()
